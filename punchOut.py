31c7cbd165d6f3a9d5e86caca2eef4e2f8504271
Ubuntu <ubuntu@ip-172-31-22-51.ec2.internal>
2021-08-02 00:42:44 +0000

    first commit

---
+++ punchOut.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+
+op = webdriver.ChromeOptions()
+op.add_argument('headless')
+driver = webdriver.Chrome('./chromedriver',options=op)
+
+
+url = 'https://qnaire.ab1.auo.com/node/4169'
+driver.get(url)
+
+user =  driver.find_element_by_name('name')
+pwd =  driver.find_element_by_name('pass')
+
+user.send_keys("jameschlee")
+pwd.send_keys("Auo2008111")
+
+login = driver.find_element_by_name('op')
+login.click()
+
+
+punchOut = driver.find_element_by_id('edit-submitted-sign-2')
+punchOut.click()
+
+punchOutNote = driver.find_element_by_id('edit-submitted-note')
+punchOutNote.send_keys("Note Test")
+
+submit = driver.find_element_by_name('op')
+submit.click()
+
+driver.close()
